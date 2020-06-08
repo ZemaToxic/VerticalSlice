95a2c6d8ccc6a79e8bef72f58a9d0fb8d90fa50d
jlu787 <jlu787@outlook.com>
2020-06-08 16:04:10 +1200

    Basic Inventory

--- Source/VerticalSlice/Public/GM_Base.h
+++ Source/VerticalSlice/Public/GM_Base.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "MonsterBase.h"
 #include "GM_Base.generated.h"
 
 /**
@@ -16,7 +17,7 @@ class VERTICALSLICE_API AGM_Base : public AGameModeBase
 	
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player")
-		TMap<FString, int32> partsCollected;
+		TMap<MonsterDrops, int32> partsCollected;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player")
 		float money;
