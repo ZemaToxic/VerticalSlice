83cc4c12910514698ca6d288fc835229dfc5c621
jason <jlu787@outlook.com>
2020-07-09 11:38:23 +1200

    Pickups are floating and magnetic

--- Source/VerticalSlice/Private/DropsBase.cpp
+++ Source/VerticalSlice/Private/DropsBase.cpp
@@ -3,7 +3,8 @@
 
 #include "DropsBase.h"
 #include "Components/StaticMeshComponent.h"
-
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 ADropsBase::ADropsBase()
@@ -13,6 +14,31 @@ ADropsBase::ADropsBase()
 
 	DropMesh = CreateDefaultSubobject<UStaticMeshComponent>("StaticMesh");
 	SetRootComponent(DropMesh);
+
+	PickupSphereComp = CreateDefaultSubobject<USphereComponent>("PickupRadius");
+	PickupSphereComp->SetupAttachment(RootComponent);
+	PickupSphereComp->InitSphereRadius(400.0f);
+
+	MagnetSphereComp = CreateDefaultSubobject<USphereComponent>("MagnetRadius");
+	MagnetSphereComp->SetupAttachment(RootComponent);
+	MagnetSphereComp->InitSphereRadius(2000.0f);
+	MagnetSphereComp->ShapeColor = FColor::Green;
+
+	
+}
+
+void ADropsBase::TimelineProgress(float Value)
+{
+	if (!CheckInRange())
+	{
+		FVector NewLoc = FMath::Lerp(StartLoc, EndLoc, Value);
+		SetActorLocation(NewLoc);
+	}
+	else
+	{
+		StartLoc = EndLoc = GetActorLocation();
+		EndLoc.Z += ZOffset;
+	}
 }
 
 // Called when the game starts or when spawned
@@ -20,6 +46,29 @@ void ADropsBase::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	// floating functionality using timelines
+	if (CurveFloat)
+	{
+		FOnTimelineFloat TimelineProgress;
+		TimelineProgress.BindUFunction(this, FName("TimelineProgress"));
+		CurveTimeline.AddInterpFloat(CurveFloat, TimelineProgress);
+		CurveTimeline.SetLooping(true);
+
+		StartLoc = EndLoc = GetActorLocation();
+		EndLoc.Z += ZOffset;
+
+		CurveTimeline.PlayFromStart();
+	}
+}
+
+// Function used to check if the drop is within range of the player using the MagnetSphereComp radius
+bool ADropsBase::CheckInRange()
+{
+	ACharacter* PlayerCharacter = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+
+	float DistToPlayer = FVector::Dist(GetActorLocation(), PlayerCharacter->GetActorLocation());
+
+	return (DistToPlayer<=MagnetSphereComp->GetScaledSphereRadius());
 }
 
 // Called every frame
@@ -27,5 +76,6 @@ void ADropsBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	CurveTimeline.TickTimeline(DeltaTime);
 }
 
--- Source/VerticalSlice/Public/DropsBase.h
+++ Source/VerticalSlice/Public/DropsBase.h
@@ -3,8 +3,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/TimelineComponent.h"
 #include "GameFramework/Actor.h"
+
+#include "MonsterBase.h"
+
 #include "DropsBase.generated.h"
+
+class UCurveFloat;
 
 UCLASS()
 class VERTICALSLICE_API ADropsBase : public AActor
@@ -15,12 +21,42 @@ public:
 	// Sets default values for this actor's properties
 	ADropsBase();
 
+	// PROPERTIES
 	UPROPERTY(EditAnywhere, Category = "Components")
 		class UStaticMeshComponent* DropMesh;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+		class USphereComponent* PickupSphereComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+		class USphereComponent* MagnetSphereComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Drops")
+		MonsterDrops DropType;
+
+	// FUNCTIONS
+	UFUNCTION()
+		void TimelineProgress(float Value);
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable)
+		bool CheckInRange();
+
+	FTimeline CurveTimeline;
+
+	UPROPERTY(EditAnywhere, Category = "Timeline")
+		UCurveFloat* CurveFloat;
+
+	UPROPERTY()
+		FVector StartLoc;
+
+	UPROPERTY()
+		FVector EndLoc;
+
+	UPROPERTY(EditAnywhere, Category = "Timeline")
+		float ZOffset;
 
 public:	
 	// Called every frame
