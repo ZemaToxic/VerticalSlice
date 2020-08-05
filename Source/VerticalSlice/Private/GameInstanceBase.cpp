c0966986449c327cb988f6ca8275925d29a34ef9
jason <jlu787@outlook.com>
2020-08-05 17:59:17 +1200

    Spikes actually delete now
    -removed some debug messages

--- Source/VerticalSlice/Private/GameInstanceBase.cpp
+++ Source/VerticalSlice/Private/GameInstanceBase.cpp
@@ -9,62 +9,13 @@
 #define VALUE_ROUGHHIDE 300
 #pragma endregion
 
-///*
-//	Sells all the parts the player has collected and converts it to money
-//
-//	@params void
-//	@return void
-//	@author Jason Lu
-//*/
-//void UGameInstanceBase::SellAllParts()
-//{
-//	for (auto& Part : partsCollected)
-//	{
-//		switch (Part.Key)
-//		{
-//		case MonsterDrops::SHARPFANG:
-//			PlayerMoney += VALUE_SHARPFANG * Part.Value;
-//			break;
-//		case MonsterDrops::HARDENEDSPIKE:
-//			PlayerMoney += VALUE_HARDSPIKE * Part.Value;
-//			break;
-//		case MonsterDrops::ROUGHHIDE:
-//			PlayerMoney += VALUE_ROUGHHIDE * Part.Value;
-//			break;
-//		default:
-//			break;
-//		}
-//		Part.Value = 0;
-//	}
-//}
-//
-///*
-//	Adds Monster Parts to the PartsCollected map
-//
-//	@params MonsterDrops _dropType, int _amount
-//	@return void
-//	@author Jason Lu
-//*/
-//void UGameInstanceBase::AddParts(MonsterDrops _dropType, int _amount)
-//{
-//	for (auto& Part : partsCollected)
-//	{
-//		if (_dropType == Part.Key)
-//		{
-//			Part.Value += _amount;
-//		}
-//	}
-//}
+/*
+	Sets the data to get ready for transition into the next level
 
-//void UGameInstanceBase::SetData_Implementation(TMap<MonsterDrops, int32> _partsCollected, float _PlayerMoney, MechUpgrades _LastMechUpgrade, AbilityUpgrades _LastAbilityUpgrade, GunUpgrades _LastGunUpgrade)
-//{
-//	partsCollected = _partsCollected;
-//	PlayerMoney = _PlayerMoney;
-//	LastMechUpgrade = _LastMechUpgrade;
-//	LastAbilityUpgrade = _LastAbilityUpgrade;
-//	LastGunUpgrade = _LastGunUpgrade;
-//}
-
+	@params TMap<MonsterDrops, int32>& _partsCollected, float _PlayerMoney, MechUpgrades _LastMechUpgrade, AbilityUpgrades _LastAbilityUpgrade, GunUpgrades _LastGunUpgrade
+	@return void
+	@author Jason Lu
+*/
 void UGameInstanceBase::SetData(TMap<MonsterDrops, int32>& _partsCollected, float _PlayerMoney, MechUpgrades _LastMechUpgrade, AbilityUpgrades _LastAbilityUpgrade, GunUpgrades _LastGunUpgrade)
 {
 	partsCollected = _partsCollected;
